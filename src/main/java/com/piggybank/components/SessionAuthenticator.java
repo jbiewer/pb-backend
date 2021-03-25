fa562c9a02f1c02ffa98e37fd95473714ddda8e3
Jacob Biewer <jbiewer@wisc.edu>
2021-03-24 19:45:56 -0500

    SessionAuth. documentation

--- src/main/java/com/piggybank/components/SessionAuthenticator.java
+++ src/main/java/com/piggybank/components/SessionAuthenticator.java
@@ -12,35 +12,45 @@ import javax.servlet.http.Cookie;
 import java.util.concurrent.TimeUnit;
 
 /**
- * todo
+ * Manages all authentication procedures on the server-side.
+ * Handles generation, validation, and revocation of session IDs through the FirebaseAuth API.
  */
 @Component
 public class SessionAuthenticator {
     /**
-     * todo
-     * @param sessionCookie
-     * @throws FirebaseAuthException
+     * Given a session ID, verifies the ID's authenticity through FirebaseAuth.
+     * A session ID would be valid if it hasn't expired from the initial expiration time set and it
+     * hasn't been revoked yet.
+     *
+     * @param sessionCookie Session ID from the HTTP request cookie.
+     * @throws FirebaseAuthException If the session ID is not authentic, or has been revoked.
      */
     public void validateSession(@NonNull String sessionCookie) throws FirebaseAuthException {
         FirebaseAuth.getInstance().verifyIdToken(sessionCookie, true);
     }
 
     /**
-     * todo
-     * @param sessionCookie
-     * @throws FirebaseAuthException
+     * Given a session ID, revokes the authenticity of it through FirebaseAuth.
+     * Initially attempts to verify a session cookie and retrieves the associated token. The token is then used to
+     * revoke the session entirely. Any subsequent requests using the session ID is thus unauthorized.
+     *
+     * @param sessionCookie Session ID from the HTTP request cookie.
+     * @throws FirebaseAuthException If the session ID is not authentic, or has already been revoked.
      */
     public void clearSessionAndRevoke(@NonNull String sessionCookie) throws FirebaseAuthException {
-        FirebaseToken decodedToken = FirebaseAuth.getInstance().verifySessionCookie(sessionCookie);
+        FirebaseToken decodedToken = FirebaseAuth.getInstance().verifySessionCookie(sessionCookie, true);
         FirebaseAuth.getInstance().revokeRefreshTokens(decodedToken.getUid());
     }
 
     /**
-     * todo
-     * @param token
-     * @return
-     * @throws FirebaseAuthException
-     * @throws AuthException
+     * Given a token from FirebaseAuth, generates a session ID and puts it in a cookie.
+     * Initially verifies the token's authenticity with FirebaseAuth and checks if it's expired. The session
+     * ID is then created from FirebaseAuth and then put in a cookie with an expiration of 5 days.
+     *
+     * @param token Token from the HTTP request parameter.
+     * @return Session cookie containing the session ID.
+     * @throws FirebaseAuthException If the token is invalid.
+     * @throws AuthException If the token has expired.
      */
     @NonNull
     public Cookie generateNewSession(@NonNull String token) throws FirebaseAuthException, AuthException {
@@ -51,16 +61,15 @@ public class SessionAuthenticator {
             throw new AuthException();
         }
 
-        long expiresIn = TimeUnit.DAYS.toMillis(5);
-
         // Attempt to generate a session cookie.
+        long expiresIn = TimeUnit.DAYS.toMillis(5);
         String sessionCookie;
         SessionCookieOptions options = SessionCookieOptions.builder()
                 .setExpiresIn(expiresIn)
                 .build();
         sessionCookie = FirebaseAuth.getInstance().createSessionCookie(token, options);
 
-        // Configure cookie and respond with OK.
+        // Configure and return cookie.
         Cookie cookie = new Cookie("session", sessionCookie);
         cookie.setMaxAge((int) expiresIn / 1000);
         cookie.setSecure(true);
