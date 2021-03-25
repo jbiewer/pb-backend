aa8c3841a89732a22cf39811d5b2093fd4288f8b
Jacob Biewer <jbiewer@wisc.edu>
2021-03-25 13:04:11 -0500

    Documentation

--- src/main/java/com/piggybank/controller/PBController.java
+++ src/main/java/com/piggybank/controller/PBController.java
@@ -3,17 +3,20 @@ package com.piggybank.controller;
 import com.piggybank.components.SessionAuthenticator;
 
 /**
- * todo
- * @param <R>
+ * Root class of all controllers for the PiggyBank back-end service.
+ * Contains bean instances for the respective repository as well as the session authenticator.
+ *
+ * @param <R> Respective repository type.
  */
 public abstract class PBController<R> {
     protected final R repository;
     protected final SessionAuthenticator authenticator;
 
     /**
-     * todo
-     * @param repository
-     * @param repository
+     * Constructor for Spring dependency injection.
+     *
+     * @param repository Interface for the controller-specific business logic.
+     * @param authenticator Authenticator for tokens and session IDs.
      */
     public PBController(R repository, SessionAuthenticator authenticator) {
         this.repository = repository;
